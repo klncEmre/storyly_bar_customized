43f288866e563033177121d4bf5cc891e9e153fd
klncEmre <kilince18@itu.edu.tr>
2022-06-20 17:13:48 +0300

    Stroyly implemented programmatically and with storyboard. Also both Storyly bars customized.

--- storyly_bar_customized/ViewController.swift
+++ storyly_bar_customized/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
-
+import Storyly
 class ViewController: UIViewController {
-
+    
+   let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+    
+    @IBOutlet weak var story_window: StorylyView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.story_window.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        self.story_window.rootViewController = self
+        self.story_window.delegate = self
+        self.story_window.storyGroupTextIsVisible = true
+        self.story_window.storyGroupTextColor = UIColor.systemYellow
+        self.story_window.storyGroupTextFont = UIFont.systemFont(ofSize: CGFloat(15))
+        story_window.storyGroupTextLines = 4
+
         // Do any additional setup after loading the view.
     }
 
 
+}
+extension ViewController: StorylyDelegate {
+    func storylyLoaded(_ storylyView: Storyly.StorylyView,
+                       storyGroupList: [Storyly.StoryGroup],
+                       dataSource: StorylyDataSource) {}
+    func storylyLoadFailed(_ storylyView: Storyly.StorylyView,
+                           errorMessage: String) {}
+     
 }
 
---
+++ storyly_bar_customized/storyly_bar_implementedViewController.swift
@@ -0,0 +1,49 @@
+//
+//  storyly_bar_implementedViewController.swift
+//  storyly_bar_customized
+//
+//  Created by Emre Kılınç on 20.06.2022.
+//
+
+import UIKit
+import Storyly
+
+class storyly_bar_implementedViewController: UIViewController {
+    let screenSize: CGRect = UIScreen.main.bounds
+    let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+    lazy var storylyView = StorylyView()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.bounds = view.frame.insetBy(dx: 0, dy: -35.0);
+        self.storylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        self.storylyView.rootViewController = self
+        self.view.addSubview(storylyView)
+        self.storylyView.delegate = self
+        self.storylyView.backgroundColor = UIColor.white
+        self.storylyView.translatesAutoresizingMaskIntoConstraints = false
+        self.storylyView.widthAnchor.constraint(equalToConstant: screenSize.width).isActive = true
+         //Set up delegat
+        self.storylyView.storyGroupTextColor = UIColor.systemBlue
+        self.storylyView.storyGroupIconBackgroundColor = UIColor.yellow
+        storylyView.storyGroupIconBorderColorNotSeen = [UIColor.brown,UIColor.gray]
+//        WHY FIRST STORY'S COLORS DID NOT CHANGED
+        storylyView.storyGroupSize = "custom"
+        storylyView.storyGroupIconStyling = StoryGroupIconStyling(height: CGFloat(100), width: CGFloat(screenSize.width / 4), cornerRadius: CGFloat(40))
+
+        
+        
+    }
+    
+}
+extension storyly_bar_implementedViewController: StorylyDelegate {
+    func storylyLoaded(_ storylyView: Storyly.StorylyView,
+                       storyGroupList: [Storyly.StoryGroup],
+                       dataSource: StorylyDataSource) {}
+    func storylyLoadFailed(_ storylyView: Storyly.StorylyView,
+                           errorMessage: String) {}
+    
+    
+}
